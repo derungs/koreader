58f5351543482365269a534c495350c0c5f80d67
Jörg Derungs <j.derungs@outlook.com>
2019-10-27 16:32:50 +0100

    ui works

--- plugins/sudoku.koplugin/sudokucell.lua
+++ plugins/sudoku.koplugin/sudokucell.lua
@@ -1,14 +1,61 @@
-local SudokuCell = {
+local BlitBuffer = require("ffi/blitbuffer")
+local Device = require("device")
+local Geom = require("ui/geometry")
+local GestureRange = require("ui/gesturerange")
+local InputContainer = require("ui/widget/container/inputcontainer")
+local logger = require("logger")
+
+local SudokuCell = InputContainer:new{
     fixed = nil,
     number = nil,
-    numbers = nil
+    numbers = nil,
+    dimen = nil,
+    x = nil,
+    y = nil,
+    enabled = true,
+    callback = function()
+        logger.warn("!! callback called")
+    end
 }
 
-function SudokuCell:new(o)
-    o = o or {}
-    setmetatable(o, self)
-    self.__index = self
-    return o
+function SudokuCell:init()
+    if Device:isTouchDevice() then
+        logger.warn("touch device", self.x, self.y, self.dimen)
+        self.ges_events = {
+            TapSelect = {
+                GestureRange:new{
+                    ges = "tap",
+                    range = self.dimen,
+                },
+                doc = "Select number",
+            },
+            HoldSelect = {
+                GestureRange:new{
+                    ges = "hold",
+                    range = self.dimen,
+                },
+                doc = "Hold Menu Item",
+            },
+        }
+    end
+end
+
+function SudokuCell:paintTo(bb, x, y)
+    self.dimen.x = x
+    self.dimen.y = y
+
+    logger.warn("new dimen", self.dimen)
+    bb:paintRect(x + self.dimen.w / 4, y + self.dimen.h / 4, self.dimen.w / 2, self.dimen.h / 2, BlitBuffer.COLOR_GRAY)
+end
+
+function SudokuCell:onTapSelect(arg, ges)
+    logger.warn("tapped", self.x, self.y, self.number, self.fixed, self.numbers)
+    return true
+end
+
+function SudokuCell:onHoldSelect(arg, ges)
+    logger.warn("held", self.x, self.y, self.number, self.fixed)
+    return true
 end
 
 function SudokuCell:add(n)
