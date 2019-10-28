f3b22875c0215bd80d066bc09eeafa92207c378a
Jörg Derungs <j.derungs@outlook.com>
2019-10-28 07:43:39 +0100

    partial dialog

--- plugins/sudoku.koplugin/sudokucell.lua
+++ plugins/sudoku.koplugin/sudokucell.lua
@@ -1,9 +1,64 @@
 local BlitBuffer = require("ffi/blitbuffer")
+local CenterContainer = require("ui/widget/container/centercontainer")
 local Device = require("device")
+local Font = require("ui/font")
+local FrameContainer = require("ui/widget/container/framecontainer")
 local Geom = require("ui/geometry")
 local GestureRange = require("ui/gesturerange")
+local HorizontalGroup = require("ui/widget/horizontalgroup")
 local InputContainer = require("ui/widget/container/inputcontainer")
+local Screen = Device.screen
+local Size = require("ui/size")
+local TextBoxWidget = require("ui/widget/textboxwidget")
+local UIManager = require("ui/uimanager")
+local VerticalGroup = require("ui/widget/verticalgroup")
 local logger = require("logger")
+
+local function _framed(widget, width)
+    width = width or Size.border.default
+    return FrameContainer:new{
+        padding = 0,
+        bordersize = width,
+        widget
+    }
+end
+
+local SudokuDialog = InputContainer:new{
+    size = Screen:getWidth() / 11
+}
+
+function SudokuDialog:init()
+    local box = VerticalGroup:new()
+    for i = 0,2 do
+        local line = HorizontalGroup:new()
+        for j = 1,3 do
+            line[j] = _framed(CenterContainer:new{
+                dimen = Geom:new{ w = self.size, h = self.size },
+                TextBoxWidget:new{
+                    dimen = Geom:new{
+                        w = self.size, h = self.size
+                    },
+                    text = i*3 + j,
+                    face = Font:getFace("cfont", self.size / 2)
+            }}, Size.border.thin)
+        end
+        box[i+1] = _framed(line)
+    end
+    self[1] = _framed(box)
+    self.dimen = self[1]:getSize()
+end
+
+function SudokuDialog:paintTo(bb, x, y)
+    self.dimen.x = x
+    self.dimen.y = y
+    InputContainer.paintTo(self, bb, x, y)
+end
+
+function SudokuDialog:onCloseWidget()
+    UIManager:setDirty(nil, function()
+        return "partial", self.dimen
+    end)
+end
 
 local SudokuCell = InputContainer:new{
     fixed = nil,
@@ -50,6 +105,12 @@ end
 
 function SudokuCell:onTapSelect(arg, ges)
     logger.warn("tapped", self.x, self.y, self.number, self.fixed, self.numbers)
+    local temp = SudokuDialog:new()
+    local region = temp:getSize()
+    region.x = self.dimen.x - 50
+    region.y = self.dimen.y - 50
+    UIManager:show(temp, "ui", region, region.x, region.y)
+--    UIManager:show(temp)
     return true
 end
 
